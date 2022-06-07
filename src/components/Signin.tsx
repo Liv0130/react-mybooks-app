9e75eb7637110dfd205807a92692a7ec31ff064b
Liv0130 <yjjhsh27@gmail.com>
2022-06-07 23:30:39 +0900

    Applied antd

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -1,3 +1,40 @@
+import { Button, Col, Input, Row } from "antd";
+
 export default function Signin() {
-  return <div><h1>Signin</h1></div>
+  return <Row align="middle">
+    <Col span={24}>
+      <Row>
+        <Col span={12}></Col>
+        <Col span={12}>
+          <div>My Books</div>
+          <div>Please Note Your Opinion</div>
+          <div />
+          <div>
+            Email
+            <span> *</span>
+          </div>
+          <div>
+            <Input 
+            placeholder="Email"
+            autoComplete="email"
+            name="email"/>
+          </div>
+          <div>
+            Password
+            <span> *</span>
+          </div>
+          <div>
+            <Input 
+            type="password"
+            autoComplete="current-password"
+            name="password"/>
+          </div>
+          <div>
+            <Button size="large">Sign In</Button>
+          </div>
+        </Col>
+      </Row>
+    </Col>
+  </Row>
+
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import 'antd/dist/antd.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
