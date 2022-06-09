b4c5975d00186e44fb0433fa5133c1ec760bfd14
Liv0130 <yjjhsh27@gmail.com>
2022-06-09 23:43:35 +0900

    Decorated login page

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -8,30 +8,31 @@ export default function Signin() {
         <Col span={12}></Col>
         <Col span={12}>
           <div className={styles.signin_title}>My Books</div>
-          <div>Please Note Your Opinion</div>
-          <div />
-          <div>
+          <div className={styles.signin_subtitle}>Please Note Your Opinion</div>
+          <div className={styles.signin_underline} />
+          <div className={styles.email_title}>
             Email
-            <span> *</span>
+            <span className={styles.required}> *</span>
           </div>
-          <div>
+          <div className={styles.input_area}>
             <Input 
             placeholder="Email"
             autoComplete="email"
-            name="email"/>
+            name="email" className={styles.input}/>
           </div>
-          <div>
+          <div className={styles.password_title}>
             Password
-            <span> *</span>
+            <span className={styles.required}> *</span>
           </div>
-          <div>
+          <div className={styles.input_area}>
             <Input 
             type="password"
             autoComplete="current-password"
-            name="password"/>
+            name="password"
+            className={styles.input}/>
           </div>
-          <div>
-            <Button size="large">Sign In</Button>
+          <div className={styles.button_area}>
+            <Button size="large" className={styles.button}>Sign In</Button>
           </div>
         </Col>
       </Row>
