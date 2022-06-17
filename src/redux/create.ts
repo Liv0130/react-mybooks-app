1e88e901d12d68011554a06d5aad7ad81cf17182
Liv0130 <yjjhsh27@gmail.com>
2022-06-17 20:33:52 +0900

    Tried to set middleware for connected-react-router

--- src/redux/create.ts
+++ src/redux/create.ts
@@ -2,15 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./modules/reducer";
 import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from "./modules/rootSaga";
+import { routerMiddleware } from "connected-react-router";
+import history from "../history";
 
 const create = () => {
   const sagaMiddleware = createSagaMiddleware();
 
   const store = configureStore({
     // composeWithDevtools, thunk automatically activate
-    reducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware)
-    })
+    reducer(history),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware, routerMiddleware(history))
+  })
     sagaMiddleware.run(rootSaga)
 
   return store;
