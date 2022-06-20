2b3701f42c61527bf492f8a1aec8996ddcd0c46b
Liv0130 <yjjhsh27@gmail.com>
2022-06-20 17:30:50 +0900

    Tried to input token to  store

--- src/redux/create.ts
+++ src/redux/create.ts
@@ -4,15 +4,22 @@ import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from "./modules/rootSaga";
 import { routerMiddleware } from "connected-react-router";
 import history from "../history";
+import TokenService from "../services/TokenService";
 
 const create = () => {
+  const token = TokenService.get();
   const sagaMiddleware = createSagaMiddleware();
 
   const store = configureStore({
     // composeWithDevtools, thunk automatically activate
     reducer(history),
+    {auth: {
+      token,
+      loading: false,
+      error: null;
+    }}
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware, routerMiddleware(history))
-  })
+})
     sagaMiddleware.run(rootSaga)
 
   return store;
